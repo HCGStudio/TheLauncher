6d3c9716d74c4e53a0aeae2c3f5755a85ba43d98
mahoshojoHCG <hcg@hcg.today>
2021-01-11 10:07:06 +0800

    Finally fix ci

--- build.ps1
+++ build.ps1
@@ -24,7 +24,7 @@ Copy-Item (Join-Path $loginAppBuildResult "*") $outputDir -Recurse
 #Build Main App
 Set-Location $appDir
 dotnet publish -c release  -r win-x64 -p:PublishSingleFile=true
-$mainAppBuildResult = "bin\release\net5.0\win-64\publish"
+$mainAppBuildResult = "bin\release\net5.0\win-x64\publish"
 #Clenup build result
 Remove-Item (Join-Path $mainAppBuildResult "*.xml")
 Remove-Item (Join-Path $mainAppBuildResult "*.pdb")
