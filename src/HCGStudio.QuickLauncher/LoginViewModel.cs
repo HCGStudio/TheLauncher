34dadd916cf81b290a32031f16a4de2e6f4e0e1f
mahoshojoHCG <hcg@hcg.today>
2021-01-11 15:13:07 +0800

    Add quick launcher

---
+++ src/HCGStudio.QuickLauncher/LoginViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactiveUI;
+
+namespace HCGStudio.QuickLauncher
+{
+    public class LoginViewModel : ReactiveObject
+    {
+        private string _userName = string.Empty;
+
+        public string UserName
+        {
+            get => _userName;
+            set => this.RaiseAndSetIfChanged(ref _userName, value);
+        }
+
+        private string _password = string.Empty;
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+    }
+}
